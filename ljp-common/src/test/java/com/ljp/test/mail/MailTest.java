9a5e6be89a2e04023664916ebfd1a8080e7b2e07
罗俊平 <591402399@qq.com>
2021-07-12 23:36:53 +0800

    this is a project about spring, spring boot, spring cloud, spring cloud alibaba.

--- ljp-common/src/test/java/com/ljp/test/mail/MailTest.java
+++ ljp-common/src/test/java/com/ljp/test/mail/MailTest.java
@@ -1,10 +1,15 @@
 package com.ljp.test.mail;
 
+import com.sun.mail.util.MailSSLSocketFactory;
+
 import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.MimeMessage;
 import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
+import java.util.Properties;
 
 /**
  * @author 罗俊平
@@ -14,14 +19,22 @@ import java.nio.charset.StandardCharsets;
  **/
 public class MailTest {
 
-    public static void main(String[] args) {
-        Session session = Session.getDefaultInstance(null);
+    public static void main(String[] args) throws MessagingException, GeneralSecurityException {
+        Properties properties = new Properties();
+        properties.put("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.host", "smtp.qq.com");
+        MailSSLSocketFactory sf = new MailSSLSocketFactory();
+        sf.setTrustAllHosts(true);
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.ssl.socketFactory", sf);
+        Session session = Session.getDefaultInstance(properties);
         MimeMessage mimeMessage = new MimeMessage(session);
         mimeMessage.setFrom("1362266598@qq.com");
         mimeMessage.addRecipients(Message.RecipientType.TO, "591402399@qq.com");
         mimeMessage.addRecipients(Message.RecipientType.TO, "luojunping800@163.com");
-        mimeMessage.setSubject("中文 this is a java mail !!!", StandardCharsets.UTF_8);
-        mimeMessage.setText("<h2>mail success</h2><p>邮件终于发送成功了！！！</p>", StandardCharsets.UTF_8);
+        mimeMessage.setSubject("中文 this is a java mail !!!", "UTF-8");
+        mimeMessage.setText("<h2>mail success</h2><p>邮件终于发送成功了！！！</p>", "UTF-8");
         Transport transport = session.getTransport();
         transport.connect("1362266598@qq.com", "lrl03503627905");
         transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
--- ljp-server-9091/src/test/java/com/ljp/test/socket/ServerTest.java
+++ ljp-server-9091/src/test/java/com/ljp/test/socket/ServerTest.java
@@ -1,54 +1,40 @@
 package com.ljp.test.socket;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+@SuppressWarnings("ALL")
 public class ServerTest {
 
 	public static void main(String[] args) throws IOException {
+		ExecutorService executorService = Executors.newFixedThreadPool(8);
 		ServerSocket serverSocket = new ServerSocket(9999);
 		for (; ; ) {
 			Socket socket = serverSocket.accept();
 			System.out.println("收到来自客户端（" + socket.getRemoteSocketAddress() + "）的连接请求！");
-			new Thread(() -> {
-				for (; ; ) {
-					boolean flag = false;
-					try {
-						BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
-//						bufferedReader.lines().forEach(message -> System.out.println("收到的客户端（" + socket.getRemoteSocketAddress() + "）消息是：" + message));
-						System.out.println("收到的客户端（" + socket.getRemoteSocketAddress() + "）消息是：" + bufferedReader.readLine());
-						flag = true;
-					} catch (Exception e) {
-						e.printStackTrace();
-						try {
-							socket.close();
-							break;
-						} catch (Exception exception) {
-							exception.printStackTrace();
-						}
+			executorService.execute(()->{
+				try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8)); BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));) {
+					String customerMessage = bufferedReader.readLine();
+					while(StringUtils.isNotBlank(customerMessage)){
+						System.out.println("收到的客户端（" + socket.getRemoteSocketAddress() + "）消息是：" + customerMessage);
+						// bufferedWriter.write("服务端已经成功收到客户端（" + socket.getRemoteSocketAddress() + "）的消息了！！！");
+						bufferedWriter.write("echo: " + socket.getRemoteSocketAddress());
+						bufferedWriter.newLine();
+						bufferedWriter.flush();
+						customerMessage = bufferedReader.readLine();
+						System.out.println("-------------------------------------------");
 					}
-					if (flag) {
-						try {
-							BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
-							bufferedWriter.write("服务端已经成功收到客户端（" + socket.getRemoteSocketAddress() + "）的消息了！！！");
-							bufferedWriter.newLine();
-							bufferedWriter.flush();
-						} catch (Exception e) {
-							e.printStackTrace();
-							try {
-								socket.close();
-								break;
-							} catch (Exception exception) {
-								exception.printStackTrace();
-							}
-						}
-					}
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-			}).start();
+			});
 		}
 	}
 
