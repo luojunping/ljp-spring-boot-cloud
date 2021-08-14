3551f3fd6a2da2c7a0c11e5d21257aba4a5b2111
罗俊平 <jpluo@iflytek.com>
2021-08-14 19:10:59 +0800

    this is a project about spring, spring boot, spring cloud, spring cloud alibaba.

--- ljp-common/src/test/java/com/ljp/test/mail/MailTest.java
+++ ljp-common/src/test/java/com/ljp/test/mail/MailTest.java
@@ -1,12 +1,12 @@
 package com.ljp.test.mail;
 
 import com.sun.mail.util.MailSSLSocketFactory;
-import jakarta.mail.Message;
-import jakarta.mail.MessagingException;
-import jakarta.mail.Session;
-import jakarta.mail.Transport;
-import jakarta.mail.internet.MimeMessage;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.MimeMessage;
 import java.security.GeneralSecurityException;
 import java.util.Properties;
 
