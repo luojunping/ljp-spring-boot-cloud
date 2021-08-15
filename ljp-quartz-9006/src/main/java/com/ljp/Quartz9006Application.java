8cb3bb7ba8f70695b036de66deaac3f29b9dd21f
罗俊平 <591402399@qq.com>
2021-08-15 18:03:35 +0800

    this is a project about spring, spring boot, spring cloud, spring cloud alibaba.

--- ljp-quartz-9006/src/main/java/com/ljp/Quartz9006Application.java
+++ ljp-quartz-9006/src/main/java/com/ljp/Quartz9006Application.java
@@ -2,8 +2,12 @@ package com.ljp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RefreshScope
 public class Quartz9006Application {
 
 	public static void main(String[] args) {
---
+++ ljp-quartz-9006/src/main/java/com/ljp/nacos/NacosController.java
@@ -0,0 +1,28 @@
+package com.ljp.nacos;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 罗俊平
+ * @email 591402399@qq.com
+ * @date 2021/7/22
+ * @since 1.0.0
+ **/
+@RestController
+public class NacosController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/test/nacos/config/{nacosKey}")
+    @RefreshScope
+    public String nacosConfig(@PathVariable("nacosKey") String nacosKey){
+        return environment.getProperty(nacosKey);
+    }
+
+}
