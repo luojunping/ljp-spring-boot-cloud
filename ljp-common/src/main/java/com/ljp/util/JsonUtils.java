949952031731528db6516c9488155a8a9107947c
罗俊平 <591402399@qq.com>
2021-12-25 20:38:38 +0800

    this is a project about spring, spring boot, spring cloud, spring cloud alibaba.

--- ljp-common/src/main/java/com/ljp/util/JsonUtils.java
+++ ljp-common/src/main/java/com/ljp/util/JsonUtils.java
@@ -1,9 +1,20 @@
 package com.ljp.util;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.ljp.security.UserDTO;
 import com.ljp.security.UserVO;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * json工具类
@@ -13,21 +24,11 @@ public final class JsonUtils {
 	private static final JsonMapper JM = new JsonMapper();
 
 	static {
-
+		JM.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 	}
 
 	private JsonUtils() throws IllegalAccessException {
 		throw new IllegalAccessException("JsonUtils can not be created !!!");
-	}
-
-	public static <T> T parseObject(String content, Class<T> clazz) {
-		T t = null;
-		try {
-			t = JM.readValue(content, clazz);
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}
-		return t;
 	}
 
 	public static String toJSONString(Object object) {
@@ -40,9 +41,47 @@ public final class JsonUtils {
 		return content;
 	}
 
+	public static <T> T parseObject(String content, Class<T> clazz) {
+		T t = null;
+		try {
+			t = JM.readValue(content, new TypeReference<>() {
+				@Override
+				public Type getType() {
+					return super.getType();
+				}
+			});
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return t;
+	}
+
+	public static <T> List<T> parseList(String content) {
+		List<T> l = null;
+		try {
+			l = JM.readValue(content, new TypeReference<>() {
+			});
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return l;
+	}
+
+	public static <T> T[] parseArray(String content) {
+		T[] array = null;
+		try {
+			array = JM.readValue(content, new TypeReference<>() {
+			});
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return array;
+	}
+
 	public static void main(String[] args) {
 		UserDTO userDTO = new UserDTO();
 		userDTO.setLoginName("zhangsan");
+		userDTO.setLoginTime(LocalDateTime.now());
 		String ss = JsonUtils.toJSONString(userDTO);
 		System.out.println("ss = " + ss);
 		UserVO userVO = JsonUtils.parseObject(ss, UserVO.class);
